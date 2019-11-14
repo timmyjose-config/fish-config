c917212a5960172872db3f8baea9cde0750977ad
Timmy Jose <zoltan.jose@gmail.com>
2019-11-14 21:01:22 +0530

    Updated to include Rust and Haskell support

--- config.fish
+++ config.fish
@@ -1,18 +1,38 @@
 # set up custom PATHs
-
-set -x PATH ~/Downloads/jdk-13.jdk/Contents/Home/bin $PATH
+set -x PATH ~/Library/Java/JavaVirtualMachines/openjdk-14/Contents/Home/bin $PATH
 set -x PATH ~/.cargo/bin $PATH
 set -x PATH /usr/local/Cellar/gcc/9.2.0/bin/ $PATH
 set -x PATH ~/Software/Idris2 $PATH
-set -x PATH ~/Software/zig/build $PATH
+set -x PATH ~/.local/bin $PATH
+set -x PATH ~/.ghcup/bin $PATH
+set -x PATH ~/.cabal/bin $PATH
 
-# custom aliases
+## opam config
+eval (opam env)
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
+#
+## llvm config
+set -gx LDFLAGS "-L/usr/local/opt/llvm/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 
-alias gcc=gcc-9
-alias g++=g++-9
+ssh-add ~/.ssh/mbp
 
-# rbenv config
+set -x LC_ALL en_US.UTF-8
 
-status --is-interactive; and source (rbenv init -|psub)
+set -x PATH ~/code/scripts $PATH
+alias runc=runc.sh
+alias runcpp=runcpp.sh
+alias runjava=runjava.sh
+alias runrs=runrs.sh
+alias runpy=runpy.sh
 
+## set variable for the racer vim plugin 
+set -gx RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 
+## postgres configuration
+alias pgstart="pg_ctl -D ~/.local/postgresdb -l ~/.local/postgresdb/logs/logfile start"
+alias pgstop="pg_ctl -D ~/.local/postgresdb stop"
+alias pgrestart="pg_ctl -D ~/.local/postgresdb restart"
+alias pgstatus="pg_ctl -D ~/.local/postgresdb status"
